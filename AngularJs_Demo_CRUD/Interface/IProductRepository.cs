b3aa109503bfb34746e140e6f12a6402460c71f7
buibup <b5209194@gmail.com>
2016-03-27 12:14:04 +0700

    first

---
+++ AngularJs_Demo_CRUD/Interface/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using AngularJs_Demo_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJs_Demo_CRUD.Interface
+{
+    interface IProductRepository
+    {
+        IEnumerable<TblProductList> GetAll();
+        TblProductList Get(int id);
+        TblProductList Add(TblProductList item);
+        bool Update(TblProductList item);
+        bool Delete(int id);
+    }
+}
